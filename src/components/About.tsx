7a7b34b489b6c2329201f524f71b28de11464397
Alejandro Ausina <100475206@alumnos.uc3m.es>
2025-07-19 20:00:01 +0700

    Add decorative lines to About section and update styles

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,14 +4,21 @@ import "../index.css";
 
 const About: FC = () => (
   <section className="about">
-    <div className="about-text">
-      <h2>¿Qué es Uni Summit?</h2>
-      <p>Es el evento del año, de aquí sales empresario/a millonario, vas a salir más inspirado que después de un paseo por la playa. 
-No dudes en apuntarte, si no pues nada te vas a perder el eventazo del año. Después de esto tu futuro de éxito está asegurado ;) (aquí iría una descripción mejor que esta :)
-      </p>
-    </div>
-    <div className="about-img">
-      <img src={evento} alt="evento" />
+    <div className="about-content">
+      <div className="about-lines">
+        <div className="line line-1"></div>
+        <div className="line line-2"></div>
+        <div className="line line-3"></div>
+      </div>
+      <div className="about-text">
+        <h2>¿Qué es Uni Summit?</h2>
+        <p>Es el evento del año, de aquí sales empresario/a millonario, vas a salir más inspirado que después de un paseo por la playa. 
+  No dudes en apuntarte, si no pues nada te vas a perder el eventazo del año. Después de esto tu futuro de éxito está asegurado ;) (aquí iría una descripción mejor que esta :)
+        </p>
+      </div>
+      <div className="about-img">
+        <img src={evento} alt="evento" />
+      </div>
     </div>
   </section>
 );
