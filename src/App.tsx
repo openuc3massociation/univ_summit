430697291a0599cf3893abcb633e5b2977962ac0
Alejandro Ausina <100475206@alumnos.uc3m.es>
2025-07-20 23:21:07 +0700

    Added hero, stats & bubble details

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Stats from "./components/Stats"
-import BubbleImg from "./components/BubbleImg"
 import FAQ from "./components/FAQ"
 
 const App: FC = () => (
